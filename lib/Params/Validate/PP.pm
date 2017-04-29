ca719daf3d6e984582f954c0bc704e4290992c9f
Dave Rolsky <autarch@urth.org>
2017-04-29 17:53:19 -0500

    Bump version after release

--- lib/Params/Validate/PP.pm
+++ lib/Params/Validate/PP.pm
@@ -3,7 +3,7 @@ package Params::Validate::PP;
 use strict;
 use warnings;
 
-our $VERSION = '1.27';
+our $VERSION = '1.28';
 
 use Params::Validate::Constants;
 use Scalar::Util 1.10 ();
--- lib/Params/Validate/XS.pm
+++ lib/Params/Validate/XS.pm
@@ -3,7 +3,7 @@ package Params::Validate::XS;
 use strict;
 use warnings;
 
-our $VERSION = '1.27';
+our $VERSION = '1.28';
 
 use Carp;
 
